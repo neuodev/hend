2e50679cd1e56461aa569ce1d1e542438aa3033e
Ahmed Ibrahim <ib153507@gmail.com>
2022-07-08 12:11:39 +0200

    Remove navbar

--- src/components/MainSection/index.js
+++ src/components/MainSection/index.js
@@ -1,12 +1,9 @@
 import React from "react";
-import Navbar from "../Layout/Navbar";
 
 const MainSection = () => {
   return (
     <div className="bg-gray-50 min-h-screen">
-      <div className="max-w-screen-lg mx-auto">
-        <Navbar />
-      </div>
+      <div className="max-w-screen-lg mx-auto"></div>
     </div>
   );
 };
