d8f6a1fbe9bdda6501e41a0bd1761c1c9edc14a3
Ahmed Ibrahim <ib153507@gmail.com>
2022-07-08 18:53:24 +0200

    Add server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const PORT = 9000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
