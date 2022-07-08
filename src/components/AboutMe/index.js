1a562f257081e62ee71441928231b67b317e059b
Ahmed Ibrahim <ib153507@gmail.com>
2022-07-08 18:49:14 +0200

    Add about me check

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -6,8 +6,18 @@ const AboutMe = () => {
       <h1 className="text-5xl text-center mb-10 font-semibold text-gray-800">
         About Me
       </h1>
-      <div className="max-w-screen-md">
-        <p className="leading-loose text-gray-700 text-center font-normal text-lg">
+      <div className="max-w-screen-lg grid grid-cols-12 grid-row-6 items-center justify-between">
+        <div className="col-span-12 md:col-start-7 md:col-end-13 row-span-full flex items-center justify-center h-full">
+          <div
+            className=" bg-gray-100 w-96 h-96 rounded-full shadow-2xl"
+            style={{
+              background: "url(/assets/hend.jpg)",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "40% 20%",
+            }}
+          />
+        </div>
+        <p className="leading-loose text-gray-700 text-center font-normal text-lg col-span-12 md:col-start-1 md:col-end-7 p-8 md:row-span-full">
           With <strong>more than 2 years</strong> of experience as an SEO Expert
           Ranking high competitive keywords on google, getting organic traffic
           to the website, Citation Building, and delivering growth for companies
