477316befafc695ef7ea5d6892539aeef0951759
Ahmed Ibrahim <ib153507@gmail.com>
2022-07-23 14:36:03 +0200

    Add favicon

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,10 +6,12 @@ import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import Tools from "../components/Tools";
 import Work from "../components/Work";
+import Favicon from "react-favicon";
 
 const HomePage = () => {
   return (
     <div className="bg-gray-50 w-full h-full overflow-hidden">
+      <Favicon url="/assets/favicon.png" />
       <MainSection />
       <Projects />
       <Work />
