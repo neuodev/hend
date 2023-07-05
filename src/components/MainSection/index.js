07ee8432e8ec4412a10bedc56407b145541f9fd8
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-07-05 19:13:55 +0300

    feat: revamp the main section layout

--- src/components/MainSection/index.js
+++ src/components/MainSection/index.js
@@ -3,24 +3,27 @@ import Social from "../common/Social";
 
 const MainSection = () => {
   return (
-    <div className="min-h-screen sm:h-screen">
-      <div className="grid grid-cols-12 grid-rows-6 h-full w-full">
-        <div
-          className="col-span-12 md:col-start-7 md:col-end-13 row-span-3 md:row-span-full bg-gray-100"
-          style={{
-            background: "url(/assets/hend.jpg)",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "40% 20%",
-          }}
-        />
-        <div className="col-span-12 md:col-start-1 md:col-end-7 row-span-3 md:row-span-full flex flex-col items-center justify-center pt-12">
-          <h1 className="text-5xl font-bold text-gray-800 mb-4">
+    <div className="flex items-center justify-center py-32">
+      <div className="flex items-center justify-center space-x-10">
+        <div className="flex flex-col items-center justify-center pt-12 text-center">
+          <h1 className="text-6xl font-bold text-gray-800 mb-4">
             Hend Ibarhim
           </h1>
-          <p className="text-xl font-medium  text-gray-800 mb-8 text-center">
-            Senior International SEO Specialist
+          <p className="text-xl font-medium text-gray-800 mb-4 text-center">
+            Senior International SEO Specialist & Google Ads
+          </p>
+          <p className="w-96 mb-4 font-light">
+            Unleash your global success, dominate search engines, capture
+            audiences with captivating ads, stay ahead of the curve.
           </p>
           <Social />
+        </div>
+        <div className="rounded-full shadow-2xl">
+          <img
+            src="assets/hend.jpg"
+            alt="Hend Ibrahim"
+            className="block w-96 h-96 rounded-full object-cover object-top"
+          />
         </div>
       </div>
     </div>
